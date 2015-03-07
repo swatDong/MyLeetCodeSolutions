d1eb18441026d93a0c9032b1a9f9d266d6873a6b
dongqianhao <09122SWAT@gmail.com>
2015-03-07 15:57:08 +0800

    format

--- c++/LeetCodeAlgorithms/LeetCodeAlgorithms/Solution_3_Longest_Substring_Without_Repeating_Characters.cpp
+++ c++/LeetCodeAlgorithms/LeetCodeAlgorithms/Solution_3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -38,10 +38,10 @@ public:
 	}
 };
 
-int main() {
-	Solution s;
-	cout << s.lengthOfLongestSubstring("abcabcbb") << endl;
-	return 0;
-}
+//int main() {
+//	Solution s;
+//	cout << s.lengthOfLongestSubstring("abcabcbb") << endl;
+//	return 0;
+//}
 
 //Given a string, find the length of the longest substring without repeating characters.For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
