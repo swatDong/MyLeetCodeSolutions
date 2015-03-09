8c745046c43da2be55d5ae860a5de159d6fc767d
dongqianhao <09122SWAT@gmail.com>
2015-03-09 16:13:47 +0800

    solution 8 accepted

---
+++ c++/LeetCodeAlgorithms/LeetCodeAlgorithms/Solution_7_String_to_Integer_atoi.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int atoi(string str) {
+		int len = str.length();
+		int p = 0;
+		if (p == len) return 0;
+		while (isspace(str[p]) && p != len){
+			p++;
+		}
+		if (p == len) return 0;
+		bool positive = true;
+		if (str[p] == '-') {
+			positive = false;
+			p++;
+		}
+		else if (str[p] == '+') {
+			p++;
+		}
+		if (p == len) return 0;
+		long long lans = 0;
+		while (p != len) {
+			char c = str[p];
+			p++;
+			if (c >= '0' && c <= '9') {
+				lans = lans * 10 + (c - '0');
+				if (lans > (long long)INT_MAX + 1) {
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+		if (positive) {
+			if (lans > INT_MAX) {
+				return INT_MAX;
+			}
+		}
+		else {
+
+			lans = -lans;
+			if (lans < INT_MIN) {
+				return INT_MIN;
+			}
+		}
+		return (int)lans;
+	}
+};
+
+//int main() {
+//	Solution s;
+//	cout << s.atoi(" \t\r -312we 12") << endl;
+//	return 0;
+//}
+
+//Implement atoi to convert a string to an integer.
+//
+//Hint: Carefully consider all possible input cases.If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+//
+//  Notes : It is intended for this problem to be specified vaguely(ie, no given input specs).You are responsible to gather all the input requirements up front.
+//
+//		  Update(2015 - 02 - 10) :
+//		  The signature of the C++ function had been updated.If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+//
+//		  spoilers alert... click to show requirements for atoi.
+//
+//		  Requirements for atoi :
+//		  The function first discards as many whitespace characters as necessary until the first non - whitespace character is found.Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+//
+//		  The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+//
+//		  If the first sequence of non - whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+//
+//		  If no valid conversion could be performed, a zero value is returned.If the correct value is out of the range of representable values, INT_MAX(2147483647) or INT_MIN(-2147483648) is returned.
